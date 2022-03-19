32ea68462f5d54e0d47127d48f26d89a4171e884
javad <m.g.gh9121@gmail.com>
2022-03-19 19:42:18 +0330

    add 150 coins & fix bog

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   useEffect(()=>{
     axios
     .get(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h%2C7d%2C30d"
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=300&page=1&sparkline=false&price_change_percentage=24h%2C7d%2C30d"
       )
 
     .then(
@@ -37,25 +37,27 @@ function App() {
     );
 
   return (
-    <div className="site-style">
-      <p className='header-coin'>Coin List</p>
+    <div className="bg-gray-800 text-white">
+      <p className='text-center text-2xl py-5'>Coin List</p>
       <Search change={handleChange} />
       <div>
-        <table className='m-auto'>
+        <table className='m-auto container w-auto'>
           <thead>
               <tr>
-                  <th className='w-1/6'> Img </th>
-                  <th className='w-1/6'> Name </th>
-                  <th className='w-1/6'> Price (USD)</th>
-                  <th className='w-1/6'> 24h </th>
-                  <th className='w-1/6'> 7d </th>
-                  <th className='w-1/6'> 30d </th>
+                  <th className='w-44'> Rank </th>
+                  <th className='w-44'> Img </th>
+                  <th className='w-44'> Name </th>
+                  <th className='w-44'> Price (USD)</th>
+                  <th className='w-44'> 24h </th>
+                  <th className='w-44'> 7d </th>
+                  <th className='w-44'> 30d </th>
               </tr>
           </thead>
           <tbody>
             {filterCoins.map(coin=>{
               return(
-                <Coin 
+                <Coin
+                  rank={coin.market_cap_rank} 
                   key={coin.id}
                   name={coin.name}
                   price={coin.current_price}
--- src/Coin.js
+++ src/Coin.js
@@ -1,31 +1,32 @@
 import React from 'react'
 // import './Coin.css'
 
-const Coin = ({key, name, price, symbol, image, priceChange24h, priceChange7d, priceChange30d}) => {
+const Coin = ({rank, key, name, price, symbol, image, priceChange24h, priceChange7d, priceChange30d}) => {
     return (
         <React.Fragment>
             <tr className='text-center'>
+                <td className='p-2'>{rank}</td>
                 <td className='p-2'><img className='w-8 m-auto' src={image} alt={name}></img></td>
                 <td className='p-2'>{name}</td>
                 <td className='p-2'>${price}</td>
                 { priceChange24h >0 ? (
-                <td className='p-2 text-green-300'>{priceChange24h.toFixed(2)}%</td>
+                <td className='p-2 text-green-300'>{Number(priceChange24h).toFixed(2)}%</td>
                 ) : (
-                <td className='p-2 text-red-300'>{priceChange24h.toFixed(2)}%</td>
+                <td className='p-2 text-red-300'>{Number(priceChange24h).toFixed(2)}%</td>
                 )
                 }
 
                 { priceChange7d >0 ? (
-                <td className='p-2 text-green-300'>{priceChange7d.toFixed(2)}%</td>
+                <td className='p-2 text-green-300'>{Number(priceChange7d).toFixed(2)}%</td>
                 ) : (
-                <td className='p-2 text-red-300'>{priceChange7d.toFixed(2)}%</td>
+                <td className='p-2 text-red-300'>{Number(priceChange7d).toFixed(2)}%</td>
                 )
                 }
 
                 { priceChange30d >0 ? (
-                <td className='p-2 text-green-300'>{priceChange30d.toFixed(2)}%</td>
+                <td className='p-2 text-green-300'>{Number(priceChange30d).toFixed(2)}%</td>
                 ) : (
-                <td className='p-2 text-red-300'>{priceChange30d.toFixed(2)}%</td>
+                <td className='p-2 text-red-300'>{Number(priceChange30d).toFixed(2)}%</td>
                 )
                 }
             </tr>
